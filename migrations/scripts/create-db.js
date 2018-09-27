2a563d8100432808dfa3d90ac2382bb6d4d9fcad
andrew.bednyak <bednyak.ao@gmail.com>
2018-09-27 20:29:39 +0300

    Update the createDB credentials

--- migrations/scripts/create-db.js
+++ migrations/scripts/create-db.js
@@ -9,11 +9,11 @@ const dbUser = config.database.user,
     dbName = config.database.name;
 
 console.log(
-    'postgres://zraqikgafddnza:e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db8mu5uuoaf65i/postgres'
+    'postgres://zraqikgafddnza:e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db8mu5uuoaf65i'
 );
 
 const CONNECTION_STRING =
-    'postgres://zraqikgafddnza:e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db8mu5uuoaf65i/postgres';
+    'postgres://zraqikgafddnza:e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a@ec2-54-227-240-7.compute-1.amazonaws.com:5432/db8mu5uuoaf65i';
 const client = new Client(CONNECTION_STRING);
 
 client.connect();
