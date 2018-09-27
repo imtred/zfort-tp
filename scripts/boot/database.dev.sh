cb3c54a2a91c39e6c6e622d1863672ca66f97cc6
andrew.bednyak <bednyak.ao@gmail.com>
2018-09-27 21:19:06 +0300

    Set Heroku Postgres credentials config

--- scripts/boot/database.dev.sh
+++ scripts/boot/database.dev.sh
@@ -3,4 +3,5 @@ export DB_NAME='zforttp'
 export DB_USERNAME='postgres'
 export DB_PASSWORD='postgres'
 export DB_DIALECT='postgres'
+export DB_HOST='localhost'
 export DB_PORT=5432
--- scripts/boot/database.staging.sh
+++ scripts/boot/database.staging.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export DB_NAME='zforttp'
-export DB_USERNAME='postgres'
-export DB_PASSWORD='postgres'
+export DB_NAME='db8mu5uuoaf65i'
+export DB_USERNAME='zraqikgafddnza'
+export DB_PASSWORD='e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a'
 export DB_DIALECT='postgres'
+export DB_HOST='ec2-54-227-240-7.compute-1.amazonaws.com'
 export DB_PORT=5432
