46cf4a213fb0a3b256737482bfc100cce9f0e32a
andrew.bednyak <bednyak.ao@gmail.com>
2018-09-27 21:16:33 +0300

    Revert credentials settings

--- migrations/config/index.js
+++ migrations/config/index.js
@@ -2,11 +2,11 @@ const config = require('../../api/app/config');
 
 module.exports = {
     [config.env]: {
-        username: 'zraqikgafddnza',
-        password: 'e335ac633fa965a65144d245721f83b6d957efa45ea02d18c3c5c7eb8bb8421a',
-        database: 'zforttp',
-        host: 'ec2-54-227-240-7.compute-1.amazonaws.com',
-        port: 5432,
-        dialect: 'postgres'
+        username: config.database.user,
+        password: config.database.password,
+        database: config.database.name,
+        host: config.database.host,
+        port: config.database.port,
+        dialect: config.database.dialect
     }
 };
